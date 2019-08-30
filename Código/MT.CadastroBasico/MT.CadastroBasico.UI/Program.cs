5644476e983dbe5d6840e91c98a077f77c38288b
Marco Túlio <tmarco783@gmail.com>
2019-08-30 14:04:04 -0300

    Adicionando a solução Windows Form Application

---
+++ Código/MT.CadastroBasico/MT.CadastroBasico.UI/Program.cs
@@ -0,0 +1,23 @@
+﻿using MT.CadastroBasico.UI.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MT.CadastroBasico.UI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TelaInicialView());
+        }
+    }
+}
